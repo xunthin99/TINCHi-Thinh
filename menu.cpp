49bfc8ba7d3a11b93c97cfc167311fea305e143e
xunthin99 <xthinh94@gmail.com>
2020-07-14 11:27:29 +0700

    Da sua menu xong

--- menu.cpp
+++ menu.cpp
@@ -9,7 +9,10 @@
 using namespace std;
 
 const int so_item = 6;
-const int so_itemCON= 6;
+const int so_itemLTC= 6;
+const int so_itemSV= 6;
+const int so_itemMH= 6;
+const int so_itemDIEM= 6;
 const int dong =10;
 const int cot = 50 ;
 const int Up = 72;
@@ -19,27 +22,28 @@ char thucdon [so_item][50] = {"1. QUAN LI LOP TIN CHI       ",
 			                  "2. QUAN LI SINH VIEN         ",
 			                  "3. QUAN LI MON HOC           ",
 			                  "4. QUAN LI DIEM              ",
+			                  "-----------------------------",
 			                  "5. Ket thuc chuong trinh     "};// LOI O DAY
 			                  
-char thucdonLTC [so_itemCON][50]= {"1. Them lop tin chi     ",
+char thucdonLTC [so_itemLTC][50]= {"1. Them lop tin chi     ",
 			                  "2. Xoa lop tin chi           ",
 			                  "3. Sua                       ",
 			                  "4. Xem danh sach Lop Tin Chi ",
 			                  "5. Xem DSSVDK Lop Tin Chi    ",
 			                  "6. Quay lai MENU Chinh       "};
-char thucdonSV [so_itemCON][50]= {"1. Them lop sinh vien    ",
+char thucdonSV [so_itemSV][50]= {"1. Them lop sinh vien    ",
 			                  "2. Xoa lop sinh vien         ",
 			                  "3. Sua SV                    ",
 			                  "3. Tim kiem thong tin SV     ",
 			                  "5. Xem danh sach SV cua 1 Lop",
 			                  "6. Quay lai MENU Chinh       "};
-char thucdonMH [so_itemCON][50]= {"1. Them lop mon hoc      ",
+char thucdonMH [so_itemMH][50]= {"1. Them lop mon hoc      ",
 			                  "2. Xoa mon hoc               ",
 			                  "3. Sua mon hoc               ",
 			                  "4. Dang Ki mon hoc           ",
 			                  "5. Xem danh sach mon hoc     ",
 			                  "6. Quay lai MENU Chinh       "};
-char thucdonDIEM [so_itemCON][50]= {"1. Nhap diem cho LTC       ",
+char thucdonDIEM [so_itemDIEM][50]= {"1. Nhap diem cho LTC       ",
 			                  "2. Bang Diem mon hoc             ",
 			                  "3. Chinh sua diem                ",
 			                  "4. Diem trung binh cua mot lop   ",
@@ -111,7 +115,34 @@ do {
 //
 //}
 //MENU CHA
-int menuCon(char td [so_itemCON][50]){
+int menuConLTC(char td [so_itemLTC][50]){
+	int chon;  
+    while  (1) {
+        chon = MenuDong (td); 
+        switch (chon) {
+        
+        case 1: gotoxy (50,20); 
+		        cout << "Vua chon chuc nang Them lop tin chi " << chon;
+				break;
+        case 2:gotoxy (50,20); 
+		        cout << "Vua chon chuc nang Xoa lop tin chi " << chon; 
+		         break;
+		case 3: gotoxy (50,20); 
+		        cout << "Vua chon chuc nang Sua lop tin chi " << chon;
+				break;
+		case 4: gotoxy (50,20); 
+		        cout << "Vua chon chuc nang Xem danh sach Lop Tin Chi" << chon;
+				break;
+		case 5: gotoxy (50,20); 
+		        cout << "Vua chon chuc nang Xem DSSVDK Lop Tin Chi" << chon;
+				break;
+        case so_itemLTC : return 0; 
+         }
+        Sleep (1000);
+    }
+    return 0;
+}
+int menuConSV(char td [so_itemSV][50]){
 	int chon;  
     while  (1) {
         chon = MenuDong (td); 
@@ -132,7 +163,61 @@ int menuCon(char td [so_itemCON][50]){
 		case 5: gotoxy (50,20); 
 		        cout << "Vua chon chuc nang " << chon;
 				break;
-        case so_itemCON : return 0; 
+        case so_itemSV : return 0; 
+         }
+        Sleep (1000);
+    }
+    return 0;
+}
+int menuConDIEM(char td [so_itemDIEM][50]){
+	int chon;  
+    while  (1) {
+        chon = MenuDong (td); 
+        switch (chon) {
+        
+        case 1: gotoxy (50,20); 
+		        cout << "Vua chon chuc nang " << chon;
+				break;
+        case 2:gotoxy (50,20); 
+		        cout << "Vua chon chuc nang " << chon; 
+		         break;
+		case 3: gotoxy (50,20); 
+		        cout << "Vua chon chuc nang " << chon;
+				break;
+		case 4: gotoxy (50,20); 
+		        cout << "Vua chon chuc nang " << chon;
+				break;
+		case 5: gotoxy (50,20); 
+		        cout << "Vua chon chuc nang " << chon;
+				break;
+        case so_itemDIEM : return 0; 
+         }
+        Sleep (1000);
+    }
+    return 0;
+}
+int menuConMH(char td [so_itemMH][50]){
+	int chon;  
+    while  (1) {
+        chon = MenuDong (td); 
+        switch (chon) {
+        
+        case 1: gotoxy (50,20); 
+		        cout << "Vua chon chuc nang " << chon;
+				break;
+        case 2:gotoxy (50,20); 
+		        cout << "Vua chon chuc nang " << chon; 
+		         break;
+		case 3: gotoxy (50,20); 
+		        cout << "Vua chon chuc nang " << chon;
+				break;
+		case 4: gotoxy (50,20); 
+		        cout << "Vua chon chuc nang " << chon;
+				break;
+		case 5: gotoxy (50,20); 
+		        cout << "Vua chon chuc nang " << chon;
+				break;
+        case so_itemMH : return 0; 
          }
         Sleep (1000);
     }
@@ -147,25 +232,25 @@ int main (){
 		        cout << "Vua chon chuc nang " << chon;
 		        Sleep (1000);
 		        system("cls");
-		        menuCon(thucdonLTC); //Da hien thi dc menu con
+		        menuConLTC(thucdonLTC); //Da hien thi dc menu con
 			    break;
         case 2:gotoxy (50,20); 
 		        cout << "Vua chon chuc nang " << chon;
 		        Sleep (1000);
 		        system("cls");
-		        menuCon(thucdonSV);
+		        menuConSV(thucdonSV);
 		        break;
 		case 3: gotoxy (50,20); 
 		        cout << "Vua chon chuc nang " << chon;
 		        Sleep (1000);
 		        system("cls");
-		        menuCon(thucdonMH);
+		        menuConMH(thucdonMH);
 				break;
 		case 4: gotoxy (50,20); 
 		        cout << "Vua chon chuc nang " << chon;
 		        Sleep (1000);
 		        system("cls");
-		        menuCon(thucdonDIEM);
+		        menuConDIEM(thucdonDIEM);
 				break;
         case so_item  : return 0; 
          }
